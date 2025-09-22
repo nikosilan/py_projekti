90d69705fcf3cf064a10beb1c046f30dcc8b35ec
juuso <juuso.kaukko@gmail.com>
2025-09-22 21:17:11 +0300

    muokkasin sql-komennon kirjoitusasua

---
+++ random_kohde_lentokenttä.py
@@ -0,0 +1,39 @@
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='Juuso',
+    password='salasana',
+    autocommit=True
+)
+
+# lista, johon funktio sijoittaa random lentokenttien nimet
+destinations = []
+
+# valitsee sattumanvaraisesti 3 lentokenttää
+def random_kohteet():
+
+    for kohde in range(3):
+        sql = f"SELECT name FROM airport ORDER BY RAND() LIMIT 1"
+        kursori = yhteys.cursor()
+        kursori.execute(sql)
+        tulos = kursori.fetchone()
+
+        #purkaa tuplen ja lisää kenttien nimet listaan destinations
+        lentokentan_nimi = tulos[0]
+        destinations.append(lentokentan_nimi)
+
+    return
+
+# tulostaa listan nätisti numeroituna
+def tulosta_numeroitu_lista(lista):
+    for indeksi, alkio in enumerate(lista, start=1):
+        print(f"{indeksi}. {alkio}")
+    return
+
+random_kohteet()
+tulosta_numeroitu_lista(destinations)
+
+
