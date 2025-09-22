a163ccd6773a01b577f70bfe5a47cd30ea895117
Nikosilan <nikos@gmail.com>
2025-09-22 16:01:37 +0300

    tarkistetaan onko käyttäjä yhdistetty

--- Main.py
+++ Main.py
@@ -1,35 +1,45 @@
 import mysql.connector
 
-# Kysytään käyttäjänimi
 nimi = input("Käyttäjä: ")
 
-# Tarkistetaan käyttäjänimi ja luodaan oikea yhteys
-if nimi == "niko":
-    yhteys = mysql.connector.connect(
-        host='127.0.0.1',
-        port=3306,
-        database='flight_game',
-        user='niko',
-        password='salasana',
-        autocommit=True
-    )
+yhteys = None
 
-elif nimi == "Juuso":
-    yhteys = mysql.connector.connect(
-        host='127.0.0.1',
-        port=3306,
-        database='flight_game',
-        user='Juuso',
-        password='salasana',
-        autocommit=True
-    )
+try:
+    # Tarkistetaan käyttäjänimi ja luodaan oikea yhteys
+    if nimi.lower() == "niko":
+        yhteys = mysql.connector.connect(
+            host='127.0.0.1',
+            port=3306,
+            database='flight_game',
+            user='niko',
+            password='salasana',
+            autocommit=True
+        )
 
-elif nimi == "illia" or nimi == "Illia":
-    yhteys = mysql.connector.connect(
-        host='127.0.0.1',
-        port=3306,
-        database='flight_game',
-        user='illia',
-        password='salasana3',
-        autocommit=True
-    )
+    elif nimi.lower() == "juuso":
+        yhteys = mysql.connector.connect(
+            host='127.0.0.1',
+            port=3306,
+            database='flight_game',
+            user='Juuso',
+            password='salasana',
+            autocommit=True
+        )
+
+    elif nimi.lower() == "illia":
+        yhteys = mysql.connector.connect(
+            host='127.0.0.1',
+            port=3306,
+            database='flight_game',
+            user='illia',
+            password='salasana3',
+            autocommit=True
+        )
+    else:
+        print("Tuntematon käyttäjä.")
+
+    # onko yhteys onnistunut
+    if yhteys and yhteys.is_connected():
+        print(f"Yhteys käyttäjällä {nimi} onnistui!")
+except mysql.connector.Error as err:
+    print(f"Yhteys epäonnistui: {err}")
