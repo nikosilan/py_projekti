6782c3b01ebcf005e6429b3df8a839e12995ae9c
jxckzy <mylvfee@gmail.com>
2025-09-22 15:21:25 +0300

    viimeinen commit

--- noppa_peli.py
+++ noppa_peli.py
@@ -1,7 +1,7 @@
 import random
 
-def heitä_noppa(syote):
-    if syote.lower() == "noppa":
+def heitä_noppa(syöte):
+    if syöte.lower() == "noppa":
         noppa_1 = int(random.randint(1, 6))
         noppa_2 = int(random.randint(1, 6))
         tulos = f'Heitetty {noppa_1} ja {noppa_2}'
@@ -11,8 +11,8 @@ def heitä_noppa(syote):
     return tulos
 
 def main():
-    syote = input("Anna komento (noppa): ")
-    heitä_noppa(syote)
+    syöte = input("Anna komento (noppa): ")
+    heitä_noppa(syöte)
 
 if __name__ == "__main__":
     main()
