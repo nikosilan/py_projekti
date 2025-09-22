962676cf6b532edb2677fa43c8c9398ecc5998f7
jxckzy <mylvfee@gmail.com>
2025-09-22 16:18:32 +0300

    noppa peli esimerkki koodi

---
+++ noppa_peli.py
@@ -0,0 +1,16 @@
+import random
+
+def heitä_noppa(syöte):
+    if syöte.lower() == "noppa":
+        noppa_1 = int(random.randint(1, 6))
+        noppa_2 = int(random.randint(1, 6))
+        tulos = f'Heitetty {noppa_1} ja {noppa_2}'
+        print(tulos)
+    return tulos
+
+def main():
+    syöte = input("Anna komento (noppa): ")
+    heitä_noppa(syöte)
+
+if __name__ == "__main__":
+    main()
