2511af36cf5e0a981b9bffee52adb981cf28bc16
jxckzy <mylvfee@gmail.com>
2025-09-23 12:25:52 +0300

    Tietokilpailu peli, vielä on lisättävä kysymyksiä

---
+++ tieto_kilpailu.py
@@ -0,0 +1,33 @@
+import random
+import math
+
+tietopankki = {"Kuinka monta maanosaa maailmassa on?":"7",
+               "Mikä on maailman suurin valtameri?":"Tyynimeri",
+               "Mikä on veden kemiallinen kaava?":"H2O"}
+
+pisteet = 0  # oletetaan että se on palkka
+
+def tietokilpailu_peli(tietopankki, pisteet):
+    kysymykset = list(tietopankki.items())
+
+    for _ in range(3):
+        for index in range(len(kysymykset)):
+            random_index = random.randint(0, len(kysymykset) - 1)
+            kysymys, oikea_vastaus = kysymykset[random_index]
+            for kysymys, oikea_vastaus in kysymykset:
+                print(kysymys)
+                pelaajan_vastaus = input("Anna vastaus: ").strip()
+                if pelaajan_vastaus.lower() == oikea_vastaus.lower():
+                    random_piste_määrä = random.randint(1, 10)
+                    print(f"Onnea! Vastasit oikein ja ansaitset {random_piste_määrä} pistettä.")
+                    pisteet += random_piste_määrä
+                else:
+                    print("Valitettavasti et päässyt läpi ja et ansainnut senttiäkään!")
+                    break
+
+    print(f"\nPelin lopussa sinulla on yhteensä {pisteet} pistettä.")
+
+    return pisteet # saatu palkka
+
+tietokilpailu_peli(tietopankki, pisteet)
+# korjata 3 satunnaista määrää kysymystä, se toistuu enemmän
