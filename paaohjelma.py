36e5bfca6d30ac08de94e959d9e5f144243d8dd1
Nikosilan <nikos@gmail.com>
2025-09-25 14:56:45 +0300

    noppa koodi pääohjelmaan

--- paaohjelma.py
+++ paaohjelma.py
@@ -2,6 +2,10 @@ from log_in import kirjautuminen
 from random_kohde_lentokenttä import random_kohteet
 from random_kohde_lentokenttä import tulosta_numeroitu_lista
 
+from noppa import noppa_peli
+
+# peli_kayttaja.py
+from noppa import noppa_peli
 
 #yhdistäminen tietokantaan
 yhteys = kirjautuminen()
@@ -11,3 +15,9 @@ kohteet = random_kohteet(yhteys)
 
 # tulostaa listan nätisti numeroituna
 tulosta_numeroitu_lista(kohteet)
+
+
+#noppapeli
+pisteet = 10  # aloitetaan esimerkiksi 10 pisteestä
+pisteet = noppa_peli(pisteet)
+print(f"Uudet pisteet: {pisteet}")
