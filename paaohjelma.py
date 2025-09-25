c1dc4a1bd1ff9aadc21c983fe01ba32665313ec3
Nikosilan <nikos@gmail.com>
2025-09-25 13:04:56 +0300

    pääohjelman alun luonti, kirjautuminen ja tulostus tietokannasta

---
+++ paaohjelma.py
@@ -0,0 +1,13 @@
+from log_in import kirjautuminen
+from random_kohde_lentokenttä import random_kohteet
+from random_kohde_lentokenttä import tulosta_numeroitu_lista
+
+
+#yhdistäminen tietokantaan
+kirjautuminen()
+
+# valitsee sattumanvaraisesti 3 isoa lentokenttää ja PALAUTTAA listan
+random_kohteet()
+
+# tulostaa listan nätisti numeroituna
+tulosta_numeroitu_lista()
