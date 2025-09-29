b22311881b9352d95686a191007d5d745f4b48d6
Nikosilan <nikos@gmail.com>
2025-09-29 13:29:48 +0300

    lisätty palkki funktio

--- lataus.py
+++ lataus.py
@@ -1,8 +1,15 @@
 import sys, time
 
-for i in range(0, 101, 2):
-    bar = "█" * (i // 5) + "░" * (20 - i // 5)
-    sys.stdout.write(f"\rAloitetaan ohjelmaa: [{bar}] {i}%")
-    sys.stdout.flush()
-    time.sleep(0.1)
-print("\nValmis!")
+def palkki():
+    for i in range(0, 101, 2):
+        bar = "█" * (i // 5) + "░" * (20 - i // 5)
+        sys.stdout.write(f"\rAloitetaan ohjelmaa: [{bar}] {i}%")
+        sys.stdout.flush()
+        time.sleep(0.1)
+    print("\nValmis!")
+
+
+
+
+
+
