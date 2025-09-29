87f3fc1cd6ca2d110239439f4b9cc5cf5334c5d0
Nikosilan <nikos@gmail.com>
2025-09-29 14:25:39 +0300

    lisätty kaupungin valinta

--- paaohjelma.py
+++ paaohjelma.py
@@ -10,11 +10,13 @@ from tieto_kilpailu import tietokilpailu_peli
 from hahmon_nimen_luonti import nimea_hahmo
 from lataus import palkki
 
+
+
 #yhdistäminen tietokantaan
 yhteys = kirjautuminen()
 
 #lataus palkki
-palkki()
+#palkki()
 
 #hahmon nimen luonti
 
@@ -32,32 +34,42 @@ if yhteys:
 # valitsee sattumanvaraisesti 3 isoa lentokenttää ja PALAUTTAA listan
 kohteet = random_kohteet(yhteys)
 
+
 # tulostaa listan nätisti numeroituna
 
 tulosta_numeroitu_lista(kohteet)
 
 
+kysymys = input("Valitse kohde kirjoittamalla numero 1, 2 tai 3: ")
+# muunnetaan syöte kokonaisluvuksi 
+try:
+    valinta = int(kysymys) - 1  
+    if valinta in [0, 1, 2]:
+        sana = kohteet[valinta]   # tallennetaan valittu kohde
+        print("Valitsit kohteen:", sana)
+    else:
+        print("Virheellinen numero (valitse 1, 2 tai 3).")
+except ValueError:
+    print("Syötteen täytyy olla numero.")
 
 
 
 
 #noppapeli
-pisteet = 10  # aloitetaan esimerkiksi 10 pisteestä
-pisteet = noppa_peli(pisteet)
-print(f"Uudet pisteet: {pisteet}")
+#pisteet = 10  # aloitetaan esimerkiksi 10 pisteestä
+#pisteet = noppa_peli(pisteet)
+#print(f"Uudet pisteet: {pisteet}")
 
 
 #tietokilpailu
-pisteet = 0
-pisteet = tietokilpailu_peli(pisteet)
-print(f"Uudet pisteet pääohjelmassa: {pisteet}")
+#pisteet = 0
+#pisteet = tietokilpailu_peli(pisteet)
+#print(f"Uudet pisteet pääohjelmassa: {pisteet}")
 
 
 
 #arvo kolme kenttää
-tulosta_numeroitu_lista(kohteet)
-
-
+#tulosta_numeroitu_lista(kohteet)'
 
 
 
