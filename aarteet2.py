012af25686d9993bdea4b07216147738b47308ea
juuso <juuso.kaukko@gmail.com>
2025-09-29 22:25:02 +0300

    Muokkasin aarteet koodia: Tein aarteista sanakirjan ja talletan aarretta vastaavan summan pelaajan tilille. Ryöstössä pelaaja menettää 10% rahoistaan

---
+++ aarteet2.py
@@ -0,0 +1,51 @@
+import random
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+                host='127.0.0.1',
+                port=3306,
+                database='flight_game',
+                user='Juuso',
+                password='salasana',
+                autocommit=True
+            )
+
+airport = "Lontoo"  # lentokenttä
+TREASURES = {"€10 seteli": 10,
+             "€20 seteli": 20,
+             "koru": 50,
+             "matkamuisto": 200}
+
+treasures_avaimet = ["€10 seteli", "€20 seteli", "koru", "matkamuisto"]
+random.shuffle(treasures_avaimet)
+satunnainen_avain = treasures_avaimet[0]
+
+def airport_event():
+    treasure_chance = 0.15  # 15% mahdollisuus löytää aarre
+    robbed_chance = 0.10    # 10% mahdollisuus tulla ryöstetyksi
+
+    roll = random.random()
+    if roll < treasure_chance:
+        item = satunnainen_avain
+
+        sql = "UPDATE game SET raha = raha + %s WHERE id = %s;"
+        kursori = yhteys.cursor()
+        kursori.execute(sql, (TREASURES[satunnainen_avain], 1))
+        yhteys.commit()
+        return f"Löysit aarteen: {item} joka on {TREASURES[satunnainen_avain]}€ arvoinen! Summa talletetaan tilillesi"
+
+    elif roll < treasure_chance + robbed_chance:
+
+        sql = "UPDATE game SET raha = raha * 0.9 WHERE id = 1;"
+        kursori = yhteys.cursor()
+        kursori.execute(sql)
+        yhteys.commit()
+        return "Ryöstö! Menetit 10% rahoistasi."
+    else:
+        return "Ei tapahtumaa."
+
+#funktion suoritus
+tapahtuma = airport_event()
+
+print(f"Saavuit lentokentälle: {airport}")
+print(f"Tapahtuma: {tapahtuma}")
