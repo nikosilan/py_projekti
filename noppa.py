eb480a3526dd02b1dcc030081d5e9d7bb88b60e7
juuso <juuso.kaukko@gmail.com>
2025-10-01 11:54:38 +0300

    Muokkasin nopan toimimaan rahan kanssa

--- noppa.py
+++ noppa.py
@@ -1,9 +1,20 @@
 # noppa_peli
 import random
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+                host='127.0.0.1',
+                port=3306,
+                database='flight_game',
+                user='Juuso',
+                password='salasana',
+                autocommit=True
+            )
+
 
 def noppa_peli(pisteet):
-    tietokone_noppa1 = random.randint(1, 6)
-    tietokone_noppa2 = random.randint(1, 6)
+    tietokone_noppa1 = random.randint(1, 1)
+    tietokone_noppa2 = random.randint(1, 1)
 
     print(f"Tietokone heitti: {tietokone_noppa1} ja {tietokone_noppa2}")
 
@@ -11,18 +22,25 @@ def noppa_peli(pisteet):
         syote = input("Haluatko heittää noppia? (y/n): ").lower()
 
         if syote == "y":
-            pelaaja_noppa1 = random.randint(1, 6)
-            pelaaja_noppa2 = random.randint(1, 6)
+            pelaaja_noppa1 = random.randint(1, 1)
+            pelaaja_noppa2 = random.randint(1, 1)
 
             print(f"Sinun heittosi: {pelaaja_noppa1} ja {pelaaja_noppa2}")
 
             if (pelaaja_noppa1, pelaaja_noppa2) == (tietokone_noppa1, tietokone_noppa2):
-                palkinto = random.randint(1, 100)
-                print(f"Onnittelut! Heitot täsmäsivät, saat {palkinto} pistettä!")
+                palkinto = random.randint(1, 1000)
+
+                sql = "UPDATE game SET raha = raha + %s WHERE id = %s;"
+                kursori = yhteys.cursor()
+                kursori.execute(sql, (palkinto, 1))
+                yhteys.commit()
+
+                print(f"Onnittelut! Heitot täsmäsivät, saat {palkinto}€!")
                 pisteet += palkinto
+
                 return pisteet
             else:
-                print("Heitot eivät täsmänneet, ei pisteitä.")
+                print("Heitot eivät täsmänneet, ei rahaa.")
 
         elif syote == "n":
             print("Et halunnut heittää noppia, peli päättyi.")
@@ -34,7 +52,7 @@ def noppa_peli(pisteet):
 def main():
     pisteet = 0
     pisteet = noppa_peli(pisteet)
-    print(f"Lopulliset pisteet: {pisteet}")
+    print(f"Lopullinen saldo: {pisteet}")
 
 
 if __name__ == "__main__":
