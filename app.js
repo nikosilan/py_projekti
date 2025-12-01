c9916fffbfebba50110b2d6150adc089a6d880e3
Nikosilan <nikos@gmail.com>
2025-12-01 15:36:42 +0200

    lisätty nappi kenttien uudelleen hakemiseen, kirjautuminen promtilla, poistettu zoomaus kartalla kun nappia painetaan

--- app.js
+++ app.js
@@ -8,12 +8,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // LayerGroup lentokentille
 let airportLayer = L.layerGroup().addTo(map);
 
-// Pelaajan lentojen määrä
+// Kysy käyttäjänimi heti sivun latautuessa
+let username = prompt("Anna käyttäjänimesi (niko, juuso, daniel, illia):");
+if (!username) {
+    username = "niko"; // oletus
+}
+
+// Pelaajan lentojen määrä (voit halutessasi hakea GameState:sta)
 const flight_count = 7;
 
-// Hakee 3 satunnaista lentokenttää backendistä
+// Funktio hakee 3 satunnaista lentokenttää backendistä
 function loadRandomDestinations() {
-    fetch(`http://127.0.0.1:5000/api/random_destinations/${flight_count}`)
+    fetch(`http://127.0.0.1:5000/api/random_destinations/${flight_count}?username=${username}`)
         .then(res => res.json())
         .then(destinations => {
             airportLayer.clearLayers();
@@ -23,11 +29,11 @@ function loadRandomDestinations() {
                     .bindPopup(`<b>${ap.name}</b><br>ICAO: ${ap.icao}<br>${ap.country}`);
             });
         })
-        .catch(err => console.error(err));
+        .catch(err => console.error("Kohteiden haku epäonnistui:", err));
 }
 
 // Hae heti kartan latautuessa
 loadRandomDestinations();
 
-// Päivitä, kun karttaa liikutetaan
-map.on("moveend", loadRandomDestinations);
+// Päivitä, kun nappia klikataan
+document.getElementById("refreshBtn").addEventListener("click", loadRandomDestinations);
--- backend.py
+++ backend.py
@@ -1,25 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import mysql.connector
 from flask_cors import CORS
-from aircraft_utils import GameState 
+from aircraft_utils import GameState
 
 app = Flask(__name__)
 CORS(app)
 
-# MySQL-yhteys
-conn = mysql.connector.connect(
-    host="127.0.0.1",
-    port=3306,
-    database="flight_game",
-    user="niko",
-    password="salasana",
-    autocommit=True
-)
+# Käyttäjäkohtaiset asetukset
+USERS = {
+    "niko": {"user": "niko", "password": "salasana"},
+    "juuso": {"user": "Juuso", "password": "salasana"},
+    "daniel": {"user": "Daniel", "password": "DA10"},
+    "illia": {"user": "illia", "password": "salasana3"}
+}
 
-# Endpoint palauttaa kolme satunnaista kohdetta JSONina
+BASE_CONFIG = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "database": "flight_game",
+    "autocommit": True
+}
+
 @app.route("/api/random_destinations/<int:flights>")
 def random_destinations(flights):
+    username = request.args.get("username", "niko").lower()
+    user_config = USERS.get(username, USERS["niko"])
+    conn = mysql.connector.connect(**BASE_CONFIG, **user_config)
+
     kohteet = GameState.random_destination(conn, flights)
+    conn.close()
     return jsonify([
         {"icao": icao, "name": nimi, "country": maa, "lat": lat, "lon": lon}
         for icao, nimi, maa, lat, lon in kohteet
