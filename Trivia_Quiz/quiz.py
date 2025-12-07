9fec519d7de3b37924b369450afc2f05213a4b8a
juuso <juuso.kaukko@gmail.com>
2025-12-07 17:21:37 +0200

    Tietokilpailupelin lisääminen projektiin. Sisältää backendin ja html+css

---
+++ Trivia_Quiz/quiz.py
@@ -0,0 +1,30 @@
+quiz = {
+    "Mikä on Suomen pääkaupunki?": {
+        "choices" : ["Hulsinki", "Hilsinki", "Helsinki", "Halsinki"],
+        "correct" : "Helsinki"
+    },
+    "Minkä brittiläisen komediaryhmän mukaan Python-ohjelmointikieli on nimetty?" : {
+        "choices" : ["The Mighty Boosh", "French & Saunders", "Monty Python", "Mitchell & Webb"],
+        "correct" : "Monty Python"
+    },
+    "Minkä niminen pilvenpiirtäjä on Die Hard elokuvan keskiössä?" : {
+        "choices" : ["Empire State Building", "World Trade Center", "Nakatomi Plaza", "Hans Gruber Tower"],
+        "correct" : "Nakatomi Plaza"
+    },
+    "Kenen kirjoittama on Saiturin Joulu (eng. A Christmas Carol)?" : {
+        "choices" : ["Charles Dickens", "Albert Camus", "Bruce Willis" ,"Margaret Atwood"],
+        "correct" : "Charles Dickens"
+    },
+    "Kuinka monta sydäntä mustekalalla on?" : {
+        "choices" : ["1", "2", "3", "4"],
+        "correct" : "3"
+    },
+    "Mikä kasvi voi kasvaa jopa 91cm vuorokaudessa?" : {
+        "choices" : ["Bambu", "Hamppu", "Purjo" ,"Mänty"],
+        "correct" : "Bambu"
+    },
+    "Kuinka monta kertaa herkempi hajuaisti koiralla on ihmiseen verrattuna?" : {
+        "choices" : ["10", "100", "1000", "10 000"],
+        "correct" : "10 000"
+    }
+}
---
+++ Trivia_Quiz/trivia_app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template, flash, jsonify
+from Trivia_Quiz.quiz import quiz
+import random
+
+app = Flask(__name__, static_folder="static_test")
+app.secret_key = "salainenAvain"
+
+
+@app.route('/')
+def trivia_quiz():
+    # Valitaan kysymys sanakirjasta
+    question = random.choice(list(quiz.keys()))
+    data = quiz[question]
+
+    # Lähetetään kysymys ja vaihtoehdot HTML:lle
+    return render_template("trivia_quiz.html",
+                           question=question,
+                           choices=data["choices"])
+
+
+@app.route("/answer", methods=["POST"])
+def answer():
+    data = request.get_json()
+
+    question = data["question"]
+    user_answer = data["answer"]
+    correct_answer = quiz[question]["correct"]
+
+    return jsonify({
+        "user": user_answer,
+        "correct": correct_answer,
+        "correct_is_user": (user_answer == correct_answer)
+    })
+
+if __name__ == "__main__":
+    app.run(port=5100, debug=True)
